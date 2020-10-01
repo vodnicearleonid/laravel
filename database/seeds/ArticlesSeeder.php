37a23b5e28719669aa35e8224d11c169e914e758
vodnicearleonid <vodnicear@gmail.com>
2020-10-01 23:17:25 +0300

    17 Посев данных

---
+++ database/seeds/ArticlesSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //1MOD.
+
+    DB::insert('INSERT INTO `articles` (`name`,`text`,`img`) VALUE (?,?,?)',
+        [
+            `Blog post`,
+            `<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>`,
+            `pic.jpg`
+        ]);
+
+    //2MOD.
+
+    DB::table('articles')->insert(
+        [
+            ['name'=>"Sample Blog Post1", 'text'=>"<p>1The point of using Lorem Ipsum is that it has a more-or-less1</p>", 'img'=>'pic2.jpg'],
+            ['name'=>"Sample Blog Post2", 'text'=>"<p>2The point of using Lorem Ipsum is that it has a more-or-less2</p>", 'img'=>'pic3.jpg']
+        ]
+    );
+
+    //3MOD.
+
+
+
+    }
+}
