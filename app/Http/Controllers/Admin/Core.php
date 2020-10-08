fdfb77ecee15c0276af3c9792df707ba1b361149
vodnicearleonid <vodnicear@gmail.com>
2020-10-08 20:58:17 +0300

    Homework_Gadgets 19

--- app/Http/Controllers/Admin/Core.php
+++ app/Http/Controllers/Admin/Core.php
@@ -8,15 +8,10 @@ use DB;
 
 class Core extends Controller
 {
-    //protected static $articles;
 
     public function __construct(){
         //$this->middleware('mymiddle');
     }
-
-   /* public static function addArticles($array){
-        return self::$articles[] = $array;
-    }*/
 
     //lista cu materiale
     public function getArticles(){
@@ -26,16 +21,47 @@ class Core extends Controller
         //$articles = DB::table('gadgets')->pluck('color');
         //$articles = DB::table('articles')->count();
         //$articles = DB::table('articles')->max('id');
-        $articles = DB::table('gadgets')->select('id','processor','price')->get();
+        //$articles = DB::table('gadgets')->select('id','processor','price')->get();
+        //$articles = DB::table('gadgets')->distinct()->select('price')->get();
+        //$query = DB::table('gadgets')->distinct()->select('price');
+        //$users =$query->addSelect('producer AS alisas')->get();
+        //$gadgets = DB::table('gadgets')->select('producer')->where('id','<',4)->get();
 
-        /*DB::table('gadgets')->chunk(2, function ($articles){
-            foreach ($articles as $article){
-                Core::addArticles($article);
-            }
-        });
-        dump(self::$articles);*/ //->orderBy(???)
+        /*$gadgets = DB::table('gadgets')->select('processor','producer')
+            ->where('id','<',5)
+            ->where('producer','like','Ryzen%','or')
+            ->get();*/
 
-        dump($articles);
+        /*$gadgets = DB::table('gadgets')->select('processor','producer')
+            ->where([
+                        ['id','<',5],
+                        ['producer','like','Ryzen%','or']
+                    ])
+            ->get();*/
+
+        /*$gadgets = DB::table('gadgets')->select('processor','producer')
+            ->where('id','<',5)
+            ->where('producer','like','Socket%','or')
+            ->orWhere('id','<','2')
+            ->get();*/
+        //$gadgets = DB::table('gadgets')->whereBetween('id',[1,3])->get();
+        //$gadgets = DB::table('gadgets')->whereNotBetween('id',[1,3])->get();
+        //$gadgets = DB::table('gadgets')->whereIn('id',[2,4])->get();
+        //$gadgets = DB::table('gadgets')->whereNotIn('id',[2,4])->get();
+        //$gadgets = DB::table('gadgets')->take(1)->get();
+        //$gadgets = DB::table('gadgets')->take(3)->skip(1)->get();
+        //$gadgets = DB::table('gadgets')->take(3)->skip(1)->get();
+
+        //$gadgets = DB::table('gadgets')->insert(['producer' => 'producer test','screen' => 'screen test']);
+
+        DB::table('gadgets')->insert(
+            [
+                ['color' => 'color test1','price' => 'price test1'],
+                ['color' => 'color test2','price' => 'price test2']
+            ]
+        );
+
+        //dump($gadgets);
     }
 
     //lista cu articol
@@ -44,3 +70,8 @@ class Core extends Controller
     }
 
 }
+/*$query = DB::table('gadgets')->distinct()->select('price');
+        $users = $query->addSelect('producer')->get();*/
+
+
+
